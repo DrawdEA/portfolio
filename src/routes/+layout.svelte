51cc22acb4ac55f5a62894868173d88a5ecec710
Edward Joshua Diesta <99942301+DrawdEA@users.noreply.github.com>
2025-04-23 00:28:57 +0800

    Initialize SvelteKit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<div class="flex flex-col items-center gap-5 bg-black h-screen text-white">
+	{@render children()}
+</div>
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<div>
+    <p>Hello, I'm Edward Diesta.</p>
+    <p>A Software Developer.</p>
+</div>
+
+<div>
+    <p>Here are some of the things I'm working on.</p>
+    <ul>
+        <li>Dorm</li>
+        <li>Stuff</li>
+        <li>What</li>
+    </ul>
+</div>
+
+<div>
+    <p>These are what I use to create. innovate. reimagine. execute.</p>
+    <ul>
+        <li>Next.js</li>
+        <li>React</li>
+        <li>Svelte</li>
+        <li>SvelteKit</li>
+        <li>TailwindCSS</li>
+        <li>Zustand</li>
+    </ul>
+</div>
