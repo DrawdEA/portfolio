ec17ab2d480ea7adacb4a8e203d97cb04686f937
Edward Joshua Diesta <99942301+DrawdEA@users.noreply.github.com>
2025-05-26 21:11:06 +0800

    Fix grid

--- src/lib/components/TechTool.svelte
+++ src/lib/components/TechTool.svelte
@@ -1,6 +1,13 @@
 <script>
     import svelte from "$lib/images/logos/svelte.png";
+    import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
     let { toolName = "Tool Name", image = svelte } = $props();
+    import { onMount } from "svelte";
+
+    onMount(() => {
+        ScrollTrigger.refresh()
+    })
+    
 </script>
 
 <div class="flex flex-col justify-center items-center h-40 w-40 bg-[#252525] rounded-4xl gap-4 tool">
